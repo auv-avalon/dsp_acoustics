c071807c36bc7ea410d1751829f0754233593486
Sascha Arnold <sascha.arnold@dfki.de>
2012-04-26 00:47:13 +0200

    added test cases for cross correlation

--- test/test_zcr_xcorr.cpp
+++ test/test_zcr_xcorr.cpp
@@ -7,8 +7,12 @@
 
 #include <iostream>
 #include <cmath>
-#include "../src/zcr.h"
-#include "../src/xcorr.h"
+#include <boost/scoped_ptr.hpp>
+
+#include "dsp_acoustics/zcr.h"
+#include "dsp_acoustics/xcorr.h"
+#include "dsp_acoustics/GraphUtils.h"
+
 
 //test zero crossing rate
 BOOST_AUTO_TEST_CASE(test_zcr)
@@ -58,4 +62,81 @@ BOOST_AUTO_TEST_CASE(test_zcr)
     signal.push_back(1.0);
     zcr = dsp::zeroCrossingRate<float>(signal.data(), 6);
     BOOST_CHECK_EQUAL(zcr,1);
+}
+
+void generateSignal(std::vector<float>& sig, double resolution = 1, unsigned int offset = 0)
+{
+    for(unsigned int i = 0; i < sig.size(); i++)
+    {
+        sig[i] = (float)sin((double)(i + offset) / (resolution*M_PI));
+    }
+};
+
+//test cross correlation
+BOOST_AUTO_TEST_CASE(test_xcorr)
+{
+    boost::scoped_ptr<dsp::Xcorr> xcorr(new dsp::Xcorr(100));
+    std::vector<float> signal_1(100, 0.0f);
+    std::vector<float> signal_2(100, 0.0f);
+    
+    // one peak, gap of 20
+    signal_1[50] = 1.0f;
+    signal_2[30] = 1.0f;
+    int gap = xcorr->calculate(signal_1.data(), signal_2.data());
+    BOOST_CHECK_EQUAL(gap,20);
+    BOOST_CHECK_EQUAL(1.0,xcorr->getCorrelation());
+    signal_1[50] = 0.0f;
+    signal_2[30] = 0.0f;
+    
+    // continuous signal, gap of 2
+    generateSignal(signal_1, 0.5);
+    generateSignal(signal_2, 0.5, 2);
+    gap = xcorr->calculate(signal_1.data(), signal_2.data());
+    BOOST_CHECK_EQUAL(gap,2);
+    
+    // continuous signal, gap of 5
+    generateSignal(signal_2, 0.5, 5);
+    gap = xcorr->calculate(signal_1.data(), signal_2.data());
+    BOOST_CHECK_EQUAL(gap,5);
+    
+    // continuous signal, gap of 6 is too high, so it should select -4
+    generateSignal(signal_2, 0.5, 6);
+    gap = xcorr->calculate(signal_1.data(), signal_2.data());
+    BOOST_CHECK_EQUAL(gap,-4);
+    
+    generateSignal(signal_1, 1);
+    std::vector<float> signal_3(100, 0.0f);
+    std::vector<float> signal_4(100, 0.0f);
+   
+    // non-continuous signal, gap of 8
+    unsigned offset = 8;
+    for(unsigned i = 0; i < 20; i++)
+    {
+        signal_3[30+i]=signal_1[i];
+        signal_4[30+offset+i]=signal_1[i];
+    }
+    gap = xcorr->calculate(signal_4.data(), signal_3.data());
+    BOOST_CHECK_EQUAL(gap,8);
+
+    // non-continuous signal, gap of 20
+    signal_4.clear();
+    signal_4.resize(100, 0.0);
+    offset = 20;
+    for(unsigned i = 0; i < 20; i++)
+    {
+        signal_4[30+offset+i]=signal_1[i];
+    }
+    gap = xcorr->calculate(signal_4.data(), signal_3.data());
+    BOOST_CHECK_EQUAL(gap,20);
+    
+    // non-continuous signal, gap of 50
+    signal_4.clear();
+    signal_4.resize(100, 0.0);
+    offset = 50;
+    for(unsigned i = 0; i < 20; i++)
+    {
+        signal_4[30+offset+i]=signal_1[i];
+    }
+    gap = xcorr->calculate(signal_4.data(), signal_3.data());
+    BOOST_CHECK_EQUAL(gap,50);
 }
